768bf38a42a6a7d33aaa732ade0f203f74d4ea35
hongilywang <hongilywang@sina.com>
2019-12-11 19:00:49 +0800

    开始框架的开发，先从打印log开始（未完）

--- Plugins/DataDriven/Source/DataDriven/Public/DDCommon/DDTypes.h
+++ Plugins/DataDriven/Source/DataDriven/Public/DDCommon/DDTypes.h
@@ -15,3 +15,20 @@ class DATADRIVEN_API UDDTypes : public UObject
 	GENERATED_BODY()
 	
 };
+
+//框架自定义Debug输出类
+#pragma region LogDebug
+
+class DATADRIVEN_API DDRecord
+{
+private:
+	//自身单例
+	static TSharedPtr<DDRecord> RecordInst;
+
+public:
+
+	//构造函数
+
+};
+
+#pragma endregion
